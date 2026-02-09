bd8cf809f9d071f54a3c75ef9301a7d19686dcb5
Md. Rishan Ahmed <166410540+Rishan757@users.noreply.github.com>
2026-02-09 08:46:34 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+// JavaScript ES6 variable declaretion
+
+let x  = 11;
+for(let x = 1; x<=10; x++){
+    // console.log(x);
+}
+
+console.log(x);
+
+let a  = 20;
+let y  = 30;
+
+let sum = a + y;
+
+console.log(`The sum is ${sum}`);
+
+
+// JavaScript ES6 concatination declaretion
+let name  = "Rishan Ahmed";
+
+let message = `I am ${name} i am a cse student and frontend developer`;
+
+console.log(message);
+ 
+
+
+// JavaScript ES6 function declaretion
+const multiply = (x,y) =>{return x * y;};
+const result = multiply(25,25);
+console.log(`The result is ${result}`);
